2e7356a3b6bde563da5744abcaec2b69763595d2
Harshit123-233 <yashhello22@gmail.com>
2026-06-07 15:35:48 +0530

    Day 1: Challenge Completed

---
+++ day1Challenge.py
@@ -0,0 +1,5 @@
+var1 = input("Enter Your Name: ")
+var2 = int(input("Enter Your Age: "))
+var3 = input("What's your Favorite Programming Language?: ")
+
+print(f"Hello {var1}! You are {var2} years old and your favorite language is {var3}.")
